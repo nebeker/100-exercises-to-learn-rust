7e3218ffdbba6280e7fe3bb6b696d5aa1d938227
Nuno Nebeker <nebeker@users.noreply.github.com>
2024-07-26 18:10:53 +0100

    Clone & Copy.

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,11 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+use std::ops::Add;
+
+//3rd printing  test results needs Debug
+//5th Clone for Copy
+#[derive(Debug, Clone)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +13,27 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+//first Add
+impl Add for WrappingU32 {
+    type Output = WrappingU32;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        WrappingU32 {
+            value: self.value + rhs.value,
+        }
+    }
+}
+
+//4th coy for multiple additions
+impl Copy for WrappingU32 {}
+
+//2nd assert_eq needs PartialEq
+impl PartialEq for WrappingU32 {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
     }
 }
 
